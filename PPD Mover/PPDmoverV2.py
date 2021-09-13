59e003619cab43dfe8750b531e972feb1f5d6c37
UCSA <32883911+UnoCheSiAnnoia@users.noreply.github.com>
2021-09-14 00:30:31 +0200

    Fixed image, fixed styling, added button
    The button still does not have any use, the image had a mistake in the writing so i fixed it, i also changed the layout of the buttons

--- PPD Mover/PPDmoverV2.py
+++ PPD Mover/PPDmoverV2.py
@@ -8,7 +8,6 @@ import zipfile
 from tkinter import font
 from PIL import ImageTk, Image
 
-
 # MAIN WINDOW SETTINGS
 gui = tk.Tk()
 gui.geometry("450x450")
@@ -20,21 +19,20 @@ Underlined = font.Font(gui, "TkDefaultFont")
 Underlined.configure(underline=True)
 
 # canvas setup
-# noinspection LongLine
 canv = tk.Canvas(gui, width=450, height=450, bg="#4f5454", bd=0, highlightcolor="#4f5454", highlightbackground="#4f5454")
 Miku1 = Image.open(os.path.join(sys.path[0], "IMG", "Hatsune miku ppd 1.png"))
 MikuImage = ImageTk.PhotoImage(Miku1)
 backgroundMiku = canv.create_image(250, 250, image=MikuImage)
-selectDirOutline = canv.create_text(79, 28, text="Select Directory", font=Underlined, fill="#4b5676")
-selectDirText = canv.create_text(80, 29, text="Select Directory", font=Underlined, fill="White")
-selZipOutline = canv.create_text(79, 128, text="Select .zip file", font=Underlined, fill="#4b5676")
-selZipText = canv.create_text(80, 129, text="Select .zip file", font=Underlined, fill="White")
-selMp4Outline = canv.create_text(79, 228, text="Select the mp4", font=Underlined, fill="#4b5676")
-selMp4Text = canv.create_text(80, 229, text="Select the mp4", font=Underlined, fill="White")
-foldTextOutline = canv.create_text(242, 228, text="Select the song folder", font=Underlined, fill="#4b5676")
-selFoldText = canv.create_text(243, 229, text="Select the song folder", font=Underlined, fill="White")
-removeZipOutline = canv.create_text(74, 381, text="Delete .zip", fill="#4b5676")
-removeZipText = canv.create_text(75, 382, text="Delete .zip", fill="White")
+selectDirOutline = canv.create_text(76, 28, text="Select Directory", font=Underlined, fill="#4b5676")
+selectDirText = canv.create_text(77, 29, text="Select Directory", font=Underlined, fill="White")
+selZipOutline = canv.create_text(76, 128, text="Select .zip file", font=Underlined, fill="#4b5676")
+selZipText = canv.create_text(77, 129, text="Select .zip file", font=Underlined, fill="White")
+selMp4Outline = canv.create_text(76, 228, text="Select the mp4", font=Underlined, fill="#4b5676")
+selMp4Text = canv.create_text(77, 229, text="Select the mp4", font=Underlined, fill="White")
+foldTextOutline = canv.create_text(252, 228, text="Select the song folder", font=Underlined, fill="#4b5676")
+selFoldText = canv.create_text(253, 229, text="Select the song folder", font=Underlined, fill="White")
+removeZipOutline = canv.create_text(65, 381, text="Delete .zip", fill="#4b5676")
+removeZipText = canv.create_text(64, 382, text="Delete .zip", fill="White")
 
 
 canv.place(x=0, y=0)
@@ -195,17 +193,14 @@ def songsdirsave():
 
 getstartingdirpath()
 
-# noinspection LongLine
 songsSelectEntry = tk.Entry(gui, textvariable=songsPath, state=DISABLED, disabledbackground="#5a676b", disabledforeground="white")
-songsSelectEntry.place(x=20, y=40)
+songsSelectEntry.place(x=15, y=40)
 
-# noinspection LongLine
 songsFolderFind = tk.Button(gui, text="select", command=getsongsdirpath, activebackground="#55d1d0", background="#87e5cf")
-songsFolderFind.place(x=160, y=36)
+songsFolderFind.place(x=16, y=68)
 
-# noinspection LongLine
 songsFolderSave = tk.Button(gui, text="save folder", command=songsdirsave, activebackground="#55d1d0", background="#87e5cf")
-songsFolderSave.place(x=20, y=68)
+songsFolderSave.place(x=160, y=36)
 
 # ZIP FILE SELECTION
 
@@ -259,7 +254,6 @@ def unzip():
             messagebox.showwarning("No location selected", "You haven't selected a directory to move the zip file to, p"
                                                            "lease select one and try again")
         else:
-            # noinspection LongLine
             messagebox.showerror("Location not found", "the location you selected to move the zip file into was not found")
 
 
@@ -271,7 +265,6 @@ def movefile():
             os.remove(zipPath.get().rstrip("\n"))
     else:
         if filezip == "":
-            # noinspection LongLine
             messagebox.showwarning("No file selected", "You have not selected a zip file, please select one and try again")
         else:
             messagebox.showerror("Zip file not found", "the selected zip file was not found by the program")
@@ -280,19 +273,17 @@ def movefile():
 # UI FOR ZIP SELECT
 
 
-# noinspection LongLine
 zipSelectEntry = tk.Entry(gui, textvariable=zipPath, state=DISABLED, disabledbackground="#5a676b", disabledforeground="white")
-zipSelectEntry.place(x=20, y=140)
+zipSelectEntry.place(x=15, y=140)
 
 zipFileFind = tk.Button(gui, text="Browse files", command=getzip, activebackground="#55d1d0", background="#87e5cf")
-zipFileFind.place(x=160, y=136)
+zipFileFind.place(x=16, y=168)  # 16, 168
 
 moveFileButton = tk.Button(gui, text="Move", command=movefile, activebackground="#55d1d0", background="#87e5cf")
-moveFileButton.place(x=20, y=168)
+moveFileButton.place(x=160, y=136)  # 160, 136
 
-# noinspection LongLine
 changeDirectory = tk.Button(gui, text="Change directory", command=getstartingfolder, activebackground="#55d1d0", background="#87e5cf")
-changeDirectory.place(x=80, y=168)
+changeDirectory.place(x=96, y=168)
 
 
 # SAVES DELETE BUTTON
@@ -305,9 +296,8 @@ def deletesaves():
 # SAVES DELETE BUTTON UI
 
 
-# noinspection LongLine
 deleteSavesButton = tk.Button(text="delete saves", command=deletesaves, activebackground="#55d1d0", background="#87e5cf")
-deleteSavesButton.place(x=20, y=322)
+deleteSavesButton.place(x=16, y=322)
 
 # REMOVE ZIP FILE
 removeZipFile.set(False)
@@ -339,9 +329,8 @@ def checkremovezip():
 
 checkremovezipinitial()
 
-# noinspection LongLine
 removeZipBox = tk.Checkbutton(variable=removeZipFile, command=checkremovezip, onvalue=True, offvalue=False, background="#4f5454", activebackground="#4f5454", selectcolor="#5a676b", fg="White", activeforeground="White")
-removeZipBox.place(x=20, y=370)
+removeZipBox.place(x=10, y=370)
 
 # VIDEO SELECT
 
@@ -374,7 +363,6 @@ def movevideo():
     video = tk.StringVar()
     song = tk.StringVar()
     if videoPath.get() == "" and selectedSongPath.get() == "":
-        # noinspection LongLine
         messagebox.showwarning("no files selected", "you need to select a video and the destination for the video before proceeding")
         return
     else:
@@ -403,19 +391,20 @@ def movevideo():
 # UI FOR THE VIDEO SELECT
 
 
-# noinspection LongLine
 selectVideoEntry = tk.Entry(gui, state=DISABLED, textvariable=videoPath, disabledbackground="#5a676b", disabledforeground="white")
-selectVideoEntry.place(x=20, y=240)
+selectVideoEntry.place(x=15, y=240)
 
-# noinspection LongLine
 selectSongEntry = tk.Entry(gui, state=DISABLED, textvariable=selectedSongPath, disabledbackground="#5a676b", disabledforeground="white")
-selectSongEntry.place(x=180, y=240)
+selectSongEntry.place(x=190, y=240)
 
 selectVideoButton = tk.Button(text="Select", command=selectvideo, activebackground="#55d1d0", background="#87e5cf")
-selectVideoButton.place(x=55, y=268)
+selectVideoButton.place(x=16, y=268)
+
+selectStartingVideoDir = tk.Button(text="Change directory", activebackground="#55d1d0", background="#87e5cf")
+selectStartingVideoDir.place(x=65, y=268)
 
 selectSongButton = tk.Button(text="Select folder", command=selectsong, activebackground="#55d1d0", background="#87e5cf")
-selectSongButton.place(x=205, y=268)
+selectSongButton.place(x=190, y=268)
 
 moveVideoButton = tk.Button(text="Move video", command=movevideo, activebackground="#55d1d0", background="#87e5cf")
 moveVideoButton.place(x=325, y=235)
